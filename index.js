4c1400df06458d2fd90d0c9c6c2da7ae1ced7f8f
JYeonDev <whddus4837@gmail.com>
2021-09-12 22:05:24 +0900

    union type, any, unknown

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-// js 문법 그대로 ts에서 사용가능
-var 이름 = "kim";
-// 변수에 타입지정가능
-var 나이 = 50;
-var 결혼 = true;
-var 회원들 = ["kim", "park"];
-var 사람들 = {
-    member1: "kim",
-    member2: "park",
-};
+// union type 타입 여러개 지정가능
+var 회원 = 123;
+회원 = "김종연";
+var 회원들 = [1, 2, 3];
+var 오브젝트 = { a: "123" };
+// any는 어떠한 타입도 들어올수 있다.
+// any를 남발하면 타입스크립트를 사용하는 의미가 없다.
+// any는 타입을 벗겨내는 용도로 사용한다.
+var 이름;
+이름 = 123;
+이름 = true;
+// any와 기능이 동일하다.
+// any보다 안전하다.
+// any보다 엄격하다
+// unknown은 연산자 사용불가
+var 성함;
+성함 = 123;
+성함 = {};
+성함 - 1;
+// union type은 새로운 타입이다.
+// string 타입 + 1 가능
+// number 타입 + 1 가능
+// string | number + 1 불가능
 // 예제 1
-var info = {
-    name: "김종연",
-    age: 27,
-    address: "인천",
-};
+var user = "kim";
+var age = undefined;
+var married = false;
+var 철수 = [user, age, married];
 // 예제2
-var favorite = {
-    singer: "Jenevieve",
-    music: "Baby Powder",
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: "Phil",
+    friend: "John",
 };
-var project = {
-    member: ["kim", "park"],
-    days: 30,
-    started: true,
-};
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
--- index.ts
+++ index.ts
@@ -1,32 +1,48 @@
-// js 문법 그대로 ts에서 사용가능
+// union type 타입 여러개 지정가능
 
-let 이름: string = "kim";
-// 변수에 타입지정가능
+let 회원: number | string = 123;
+회원 = "김종연";
 
-let 나이: number = 50;
-let 결혼: boolean = true;
-let 회원들: string[] = ["kim", "park"];
-let 사람들: { member1: string; member2: string } = {
-  member1: "kim",
-  member2: "park",
-};
+let 회원들: (number | string)[] = [1, 2, 3];
+
+let 오브젝트: { a: string | number } = { a: "123" };
+
+// any는 어떠한 타입도 들어올수 있다.
+// any를 남발하면 타입스크립트를 사용하는 의미가 없다.
+// any는 타입을 벗겨내는 용도로 사용한다.
+let 이름: any;
+이름 = 123;
+이름 = true;
+
+// any와 기능이 동일하다.
+// any보다 안전하다.
+// any보다 엄격하다
+// unknown은 연산자 사용불가
+let 성함: unknown;
+성함 = 123;
+성함 = {};
+성함 - 1;
+
+// union type은 새로운 타입이다.
+// string 타입 + 1 가능
+// number 타입 + 1 가능
+// string | number + 1 불가능
 
 // 예제 1
-let info: { name: string; age: number; address: string } = {
-  name: "김종연",
-  age: 27,
-  address: "인천",
-};
+let user: string = "kim";
+let age: undefined | number = undefined;
+let married: boolean = false;
+let 철수: (string | number | boolean | undefined)[] = [user, age, married];
 
 // 예제2
-
-let favorite: { singer: string; music: string } = {
-  singer: "Jenevieve",
-  music: "Baby Powder",
+let 학교: {
+  score: (number | boolean)[];
+  teacher: string;
+  friend: string | string[];
+} = {
+  score: [100, 97, 84],
+  teacher: "Phil",
+  friend: "John",
 };
-
-let project: { member: string[]; days: number; started: boolean } = {
-  member: ["kim", "park"],
-  days: 30,
-  started: true,
-};
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
