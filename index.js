2e7c811c03b229e2cd66f2bd8ab8fe86316f3a79
JYeonDev <whddus4837@gmail.com>
2021-09-13 22:27:18 +0900

    Literal types

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-// type alias 변수를 타입에 담아서 사용 가능하다.
-// trpe alias 관습
-// 파스칼 케이스를 사용한다.
-var 동물 = 123;
-var 과일 = { name: "kim", age: 20 };
-// javascript 에서는 const 를 사용시 오브젝트 자료 수정이 가능하지만 typescript 에서는 자료수정을 막을 수 있다.
-// readonly 를 사용시 읽기전용으로 설정하면 수정이 불가능하다. 수정시 에러가 발생한다.
-// 하지만, 에디터 & 터미널에서만 typescript  에러가 발생할 뿐 실제 컴파일되는 jsvascript파일에는 정상적으로 컴파일 되서 동작한다.
-{
-    var 여친 = {
-        name: "엠버",
-    };
-    여친.name = "유라";
-    // 같은 이름의 type 변수 재정의 불가능}
-// 같은 이름의 type 변수 재정의 불가능
+// Literal types 지정한 값만 들어오게 하여 사용할 수 있다.
+// union 으로도 지정 가능
+var 이름;
+var 접니다;
+// 함수에서 파라미터의 값을 지정가능하다.
+// return 값도 지정가능
+function 함수(a) {
+    return 1;
+}
+function 가위바위보(x) {
+    return ["가위"];
+}
+가위바위보("가위");
+// Literal type의 문제점
+var 자료 = {
+    name: "kim",
+};
+function 내함수(a) { }
+// 자료.name은 타입이 string이지만 a: 'kim'은 타입이 'kim'이다
+내함수(자료.name);
+// 해결법 object 만들 때 타입지정 확실히
+// as 문법 사용
+// as const 사용 1. object value 값을 그대로 타입으로 지정해줌 2. 모든 속성들을 readonly로 바꿔준다
--- index.ts
+++ index.ts
@@ -1,35 +1,29 @@
-// type alias 변수를 타입에 담아서 사용 가능하다.
-// trpe alias 관습
-// 파스칼 케이스를 사용한다.
+// Literal types 지정한 값만 들어오게 하여 사용할 수 있다.
+// union 으로도 지정 가능
+let 이름: 123;
+let 접니다: "대머리" | "솔로";
 
-type Animal = string | number | undefined;
-
-let 동물: Animal = 123;
-
-type Fruit = { name: string; age: number };
-let 과일: Fruit = { name: "kim", age: 20 };
-
-// javascript 에서는 const 를 사용시 오브젝트 자료 수정이 가능하지만 typescript 에서는 자료수정을 막을 수 있다.
-// readonly 를 사용시 읽기전용으로 설정하면 수정이 불가능하다. 수정시 에러가 발생한다.
-// 하지만, 에디터 & 터미널에서만 typescript  에러가 발생할 뿐 실제 컴파일되는 jsvascript파일에는 정상적으로 컴파일 되서 동작한다.
-{
-type Girlfriend = {
-  readonly name: string
+// 함수에서 파라미터의 값을 지정가능하다.
+// return 값도 지정가능
+function 함수(a: "hello"): 1 | 0 {
+  return 1;
 }
-const 여친: Girlfriend = {
-  name: "엠버",
-};
 
-여친.name = "유라";
+function 가위바위보(x: "가위" | "바위" | "보"): ("가위" | "바위" | "보")[] {
+  return ["가위"];
+}
 
-// type alias 를 합치는 것도 가능하다.
-type Name = string;
-type Age = number;
-type Person = Name | Age;
+가위바위보("가위");
 
-// &연산자로 object 타입 합치기도 가능
-type PositionX = {x : number};
-type PositionY = {y : number};
-type NewType = PositionX & PositionY;
+// Literal type의 문제점
+let 자료 = {
+  name: "kim",
+} as const;
 
-// 같은 이름의 type 변수 재정의 불가능
+function 내함수(a: "kim") {}
+// 자료.name은 타입이 string이지만 a: 'kim'은 타입이 'kim'이다
+내함수(자료.name);
+
+// 해결법 object 만들 때 타입지정 확실히
+// as 문법 사용
+// as const 사용 1. object value 값을 그대로 타입으로 지정해줌 2. 모든 속성들을 readonly로 바꿔준다
