6e9bc8d717f457b003ceac49781c113719584e2f
JYeonDev <whddus4837@gmail.com>
2021-09-14 23:06:37 +0900

    class 타입지정

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
-// HTML 조작시 narrowing 방법 5개
-// 1.if
-// let 제목 = document.querySelector("#title");
-// if (제목 != null) {
-//   제목.innerHTML = "반가워요";
-// }
-// 2.instanceof
-// let 제목 = document.querySelector("#title");
-// if (제목 instanceof Element) {
-//   제목.innerHTML = "반가워요";
-// }
-// 3. as
-// let 제목 = document.querySelector("#title") as Element;
-// 제목.innerHTML = "반가워요";
-// 4. ?
-var 제목 = document.querySelector("#title");
-if ((제목 === null || 제목 === void 0 ? void 0 : 제목.innerHTML) != undefined) {
-    제목.innerHTML = "반가워요";
-}
-// 5. tsconfig.json 에서  "strictNullChecks": false
-var 링크 = document.querySelector(".link");
-if (링크 instanceof HTMLAnchorElement) {
-    링크.href = "https://kakao.com";
-}
-var 버튼 = document.querySelector("#button");
-버튼 === null || 버튼 === void 0 ? void 0 : 버튼.addEventListener("click", function () { });
+var Person = /** @class */ (function () {
+    function Person(a) {
+        this.name = a;
+    }
+    Person.prototype.함수 = function (a) {
+        console.log(a);
+    };
+    return Person;
+}());
+var 사람1 = new Person("kim");
+var 사람2 = new Person("jong");
--- index.ts
+++ index.ts
@@ -1,31 +1,12 @@
-// HTML 조작시 narrowing 방법 5개
-// 1.if
-// let 제목 = document.querySelector("#title");
-// if (제목 != null) {
-//   제목.innerHTML = "반가워요";
-// }
-
-// 2.instanceof
-// let 제목 = document.querySelector("#title");
-// if (제목 instanceof Element) {
-//   제목.innerHTML = "반가워요";
-// }
-
-// 3. as
-// let 제목 = document.querySelector("#title") as Element;
-// 제목.innerHTML = "반가워요";
-
-// 4. ?
-let 제목 = document.querySelector("#title");
-if (제목?.innerHTML != undefined) {
-  제목.innerHTML = "반가워요";
+class Person {
+  name: string;
+  constructor(a: string) {
+    this.name = a;
+  }
+  함수(a: string) {
+    console.log(a);
+  }
 }
 
-// 5. tsconfig.json 에서  "strictNullChecks": false
-
-let 링크 = document.querySelector(".link");
-if (링크 instanceof HTMLAnchorElement) {
-  링크.href = "https://kakao.com";
-}
-let 버튼 = document.querySelector("#button");
-버튼?.addEventListener("click", function () {});
+let 사람1 = new Person("kim");
+let 사람2 = new Person("jong");
