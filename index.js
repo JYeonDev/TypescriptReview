847e4a40bbf5f0daf4047ec72ec40c01f492bd6d
JYeonDev <whddus4837@gmail.com>
2021-09-15 22:13:47 +0900

    protected static

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
-// public
-// 모든 자식들이 이용가능
+// protected private 와 유사하다. class, object안에서만 사용가능하게한다.
+// 하지만, protected 는 extends 된 class에서 사용가능하다.
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var User = /** @class */ (function () {
-    function User(a) {
-        this.name = "kim";
-        this.name = a;
+    function User() {
+        this.x = 10;
     }
     return User;
 }());
-var 유저1 = new User("park");
-유저1.name = "안뇽";
-// private
-// class 안에서만 수정, 이용가능
-var User2 = /** @class */ (function () {
-    function User2(a) {
-        this.familyName = "kim";
-        this.name = a + this.familyName;
+var NewUser = /** @class */ (function (_super) {
+    __extends(NewUser, _super);
+    function NewUser() {
+        return _super !== null && _super.apply(this, arguments) || this;
     }
-    User2.prototype.이름변경함수 = function () {
-        this.familyName = "park";
+    NewUser.prototype.dothis = function () {
+        this.x = 20;
     };
+    return NewUser;
+}(User));
+// static 부모 class에서만 사용가능하도록한다.
+// static 은 public, private, protected와 같이 사용이 가능하다.
+var User2 = /** @class */ (function () {
+    function User2() {
+        this.y = 20;
+    }
+    User2.x = 10;
     return User2;
 }());
-var 유저2 = new User2("park");
-console.log(유저2);
-유저2.이름변경함수();
-console.log(유저2);
-var Person = /** @class */ (function () {
-    function Person(name) {
-        this.name = name;
-    }
-    return Person;
-}());
-var 자식 = new Person("김");
+var 자식 = new User2();
 console.log(자식);
+// 활용
+var User3 = /** @class */ (function () {
+    function User3() {
+        this.intro = User3.skill;
+    }
+    User3.skill = "js";
+    return User3;
+}());
+var 철수 = new User3();
+console.log(철수);
+User3.skill = 'ts';
+var 철수2 = new User3();
+console.log(철수2);
--- index.ts
+++ index.ts
@@ -1,36 +1,38 @@
-// public
-// 모든 자식들이 이용가능
+// protected private 와 유사하다. class, object안에서만 사용가능하게한다.
+// 하지만, protected 는 extends 된 class에서 사용가능하다.
 
 class User {
-  public name = "kim";
-  constructor(a) {
-    this.name = a;
+  protected x = 10;
+}
+
+class NewUser extends User {
+  dothis() {
+    this.x = 20;
   }
 }
 
-let 유저1 = new User("park");
-유저1.name = "안뇽";
-
-// private
-// class 안에서만 수정, 이용가능
+// static 부모 class에서만 사용가능하도록한다.
+// static 은 public, private, protected와 같이 사용이 가능하다.
 
 class User2 {
-  name: string;
-  private familyName: string = "kim";
-  constructor(a) {
-    this.name = a + this.familyName;
-  }
-  이름변경함수() {
-    this.familyName = "park";
-  }
+  public static x = 10;
+  y = 20;
 }
-let 유저2 = new User2("park");
-console.log(유저2);
-유저2.이름변경함수();
-console.log(유저2);
 
-class Person {
-  constructor(public name: string) {}
-}
-let 자식 = new Person("김");
+let 자식 = new User2();
 console.log(자식);
+
+// 활용
+
+class User3 {
+  static skill = "js";
+  intro = User3.skill "전문가입니다";
+}
+
+let 철수 = new User3();
+console.log(철수);
+
+User3.skill = 'ts';
+
+let 철수2 = new User3();
+console.log(철수2)
