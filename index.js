06c906d0bfbc790be43d49e6a62682276ecab641
JYeonDev <whddus4837@gmail.com>
2021-09-16 22:33:53 +0900

    Generic 함수

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var 변수 = "park";
+// Generic 함수
+// 파라미터로 타입을 입혁하는 함수
+function 함수(x) {
+    return x[0];
+}
+var a = 함수([4, 2]);
+var b = 함수(["4", "2"]);
+console.log(a);
+// extends 를 사용하면 타입이 맞는지 체크 할수 있다.
+function 함수2(x) {
+    return x - 1;
+}
+var a = 함수(100);
--- index.ts
+++ index.ts
@@ -1,3 +1,17 @@
-import { Name, 나이 } from "./a";
+// Generic 함수
+// 파라미터로 타입을 입혁하는 함수
 
-let 변수: Name = "park";
+function 함수<Type>(x: Type[]): Type {
+  return x[0];
+}
+
+let a = 함수<number>([4, 2]);
+let b = 함수<string>(["4", "2"]);
+console.log(a);
+
+// extends 를 사용하면 타입이 맞는지 체크 할수 있다.
+function 함수2<Type extends number>(x: Type) {
+  return x - 1;
+}
+
+let a = 함수<number>(100);
