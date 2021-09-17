41e3d4899dd1665d59e447728b400d58cabb35b9
JYeonDev <whddus4837@gmail.com>
2021-09-17 23:12:06 +0900

    tuple type

--- index.ts
+++ index.ts
@@ -1,17 +1,11 @@
-// Generic 함수
-// 파라미터로 타입을 입혁하는 함수
+// tuple type 위치별 타입지정
 
-function 함수<Type>(x: Type[]): Type {
-  return x[0];
+let 멍멍: [string, number, boolean?] = ["dog", 123, true];
+
+function 함수(...x: [number, string]) {
+  console.log(x);
 }
+함수(111, "222");
 
-let a = 함수<number>([4, 2]);
-let b = 함수<string>(["4", "2"]);
-console.log(a);
-
-// extends 를 사용하면 타입이 맞는지 체크 할수 있다.
-function 함수2<Type extends number>(x: Type) {
-  return x - 1;
-}
-
-let a = 함수<number>(100);
+let arr = [1, 2, 3];
+let arr2: [...number[]] = [4, 5, ...arr];
